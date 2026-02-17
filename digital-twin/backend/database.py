f99e7355f7d2668bb2dba81fed00064c7b612b3f
sonya <sofiyaram@icloud.com>
2026-02-17 09:17:13 +0000

    ml

--- digital-twin/backend/database.py
+++ digital-twin/backend/database.py
@@ -13,7 +13,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-# зависимостьдля роутеров
+# зависимостьдля роутеров 
 def get_db():
     db = SessionLocal()
     try:
