8480eb4df334389460b7cd7fb6127af7b4d486fe
sonya <sofiyaram@icloud.com>
2026-02-17 08:05:41 +0000

    db

--- digital-twin/backend/database.py
+++ digital-twin/backend/database.py
@@ -1,10 +1,10 @@
-import os
+
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite 
+# sqlite 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
@@ -12,7 +12,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-# Зависимость (Dependency) для роутеров
+# зависимостьдля роутеров
 def get_db():
     db = SessionLocal()
     try:
